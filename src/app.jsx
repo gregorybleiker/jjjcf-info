423229b0a911c7e0ea90797a2c7366c89c8303d2
Gregory Bleiker <gregorybleiker@gmail.com>
2023-11-20 17:33:20 +0100

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,44 @@
+import 'bulma/css/bulma.min.css'
+import { useState } from 'preact/hooks'
+import './app.css'
+
+
+export function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <div class="card">
+        <header class="card-header">
+          <p class="card-header-title">
+            News
+          </p>
+          <button class="card-header-icon" aria-label="more options">
+            <span class="icon">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+            <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a href="#" class="card-footer-item">Save</a>
+          <a href="#" class="card-footer-item">Edit</a>
+          <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+      </div>
+      <div class="card">
+        <button class="button is-danger" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
